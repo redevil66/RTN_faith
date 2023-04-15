842e0521058ff405241c3ab7ee7611efc9bfe60e
Heath McBurnett <heath.mcburnett@gmail.com>
2023-04-15 15:14:15 -0500

    update

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
+import { urlFor } from '@/lib/client'
 
-const FooterBanner = () => {
+const FooterBanner = ({footerBanner}) => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+      <div>
+        <p className='beats-solo'>{footerBanner.smallText}</p>
+        <h3>{footerBanner.midText}</h3>
+        <h1>{footerBanner.largeText1}</h1>
+        <img src={urlFor(footerBanner.image)} alt="headphones" className='footer-banner-image'/>
+      </div>
+      <Link href={"/product/${footerBanner.product}"}>
+        <button type="button">{footerBanner.buttonText}</button>
+      </Link>
+      <div className='desc'>
+        <h5>Description</h5>
+        <p>{footerBanner.desc}</p>
+      </div>
+    </div>
   )
 }
 
--- components/Product.jsx
+++ components/Product.jsx
@@ -8,7 +8,7 @@ const Product = ({product: { image, name, slug, price}}) => {
       <Link href={'/product/${slug.current}'}>
         <div className='product-card'>
           <img src={urlFor(image && image[0])}
-          width={350}
+          width={250}
           height={250}
           className='product-image'
           />
